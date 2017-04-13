b4d991861734bb280b9679df31aada9c0dd83fce
every-sushi <nadia0515@naver.com>
2017-04-13 20:50:05 +0900

    #4 print name and tel

--- app/src/main/java/emirim0223/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/emirim0223/kr/hs/emirim/activitytest/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editname, edittel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         Button but_main=(Button)findViewById(R.id.but_main);
         but_main.setOnClickListener(butMainHandler);
         Button but_second=(Button)findViewById(R.id.but_second);
+        editname = (EditText)findViewById(R.id.edit_name);
+        edittel = (EditText)findViewById(R.id.edit_tel);
     }
 
     View.OnClickListener butMainHandler = new View.OnClickListener(){
@@ -22,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            String name = editname.getText().toString();
+            String tel = editname.getText().toString();
+            intent.putExtra("name",name);// 첫번째건 이름 두번째건 값
+            intent.putExtra("tel",tel);
             startActivity(intent);
         }
     };
